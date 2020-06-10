aac2710323164502ad01fee44e211f463b94d2d6
rgrhodesdev03 <rgrhodesdev03@gmail.com>
2020-06-10 08:47:23 +0100

    Check Variables match current environment

--- stage/vpc/main.tf
+++ stage/vpc/main.tf
@@ -20,7 +20,7 @@ terraform {
 module "environment_vpc" {
     source ="../../modules/vpc"
 
-    vpc_name = "stage"
+    vpc_name = "Stage"
     vpc_cidr = "192.168.0.0/16"
     public_subnet_a = "192.168.1.0/24"
     public_subnet_b = "192.168.2.0/24"
